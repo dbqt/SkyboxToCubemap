34b7f59704d3b5ba7a30094a957dc041e95c01d9
dbqt <6835629+dbqt@users.noreply.github.com>
2024-06-13 00:25:47 -0700

    Initial commit

---
+++ Editor/SkyboxToCubemap.cs
@@ -0,0 +1,43 @@
+using UnityEditor;
+using UnityEngine;
+
+public class SkyboxToCubemapWindow : ScriptableWizard
+{
+    public Object cameraObj;
+    public Object cubemapObj;
+
+    [MenuItem("Tools/Skybox to Cubemap")]
+    public static void SkyboxToCubemapCall()
+    {
+        ScriptableWizard.DisplayWizard<SkyboxToCubemapWindow>("Skybox Capture", "Capture");
+    }
+
+    protected override bool DrawWizardGUI()
+    {
+        GUILayout.Label("Assign a camera to capture the environment from");
+        cameraObj = EditorGUILayout.ObjectField(cameraObj, typeof(Camera), true);
+        GUILayout.Label("Assign a cubemap to overwrite with the captured environment");
+        cubemapObj = EditorGUILayout.ObjectField(cubemapObj, typeof(Cubemap), true);
+
+        return true;
+    }
+
+    private void OnWizardCreate()
+    {
+        if (cameraObj == null || cubemapObj == null)
+        {
+            EditorUtility.DisplayDialog("Error", $"The camera or the cubemap is invalid.", "OK");
+            return;
+        }
+
+        if ((cameraObj as Camera).RenderToCubemap(cubemapObj as Cubemap))
+        {
+
+            EditorUtility.DisplayDialog("Done", $"The environment has been captured into the cubemap.", "OK");
+        }
+        else
+        {
+            EditorUtility.DisplayDialog("Error", $"Your device does not support rendering to cubemap", "OK");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Skybox to Cubemap
+
+Simple tool to capture the environment from a camera to a cubemap texture.
+
+## Usage
+
+1. Open your scene
+2. Place a camera where you want the environment to be captured from
+3. Optionally create a new cubemap (you can use the included example cubemap instead) - whichever you use will be overwritten!
+4. Open the menu for `Tools > Skybox to CubeMap`
+5. Assign the camera and a cubemap to overwrite
+6. Click on `Capture`
